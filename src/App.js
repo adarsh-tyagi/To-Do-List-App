7071c5354237a4fa062461950b8c4ad21afce81a
Tyagi1999 <adarshtyagi044@gmail.com>
2021-03-10 11:52:26 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentTask: "",
+      todoList: [],
+      completedList: [],
+    };
+
+    this.changeHandler = this.changeHandler.bind(this);
+    this.addHandler = this.addHandler.bind(this);
+    this.deleteHandler = this.deleteHandler.bind(this);
+    this.inputReset = this.inputReset.bind(this);
+  }
+
+  changeHandler(e) {
+    this.setState({
+      currentTask: e.target.value,
+    });
+  }
+
+  addHandler() {
+    if (!this.state.currentTask.match(/^\s*$/)) {
+      this.setState({
+        todoList: [...this.state.todoList, this.state.currentTask],
+      });
+      this.inputReset();
+    }
+  }
+
+  deleteHandler(key) {
+    var removedItem = this.state.todoList.splice(key, 1);
+    var newComList = [...this.state.completedList, ...removedItem];
+    this.setState({
+      todoList: this.state.todoList,
+      completedList: newComList,
+    });
+    console.log(this.state.todoList);
+    console.log(this.state.completedList);
+  }
+
+  inputReset() {
+    this.setState({
+      currentTask: "",
+    });
+  }
+
+  render() {
+    return (
+      <div className="outer">
+        <div className="inner">
+          <h1 className="heading">ToDo Tasks List</h1>
+          <div className="input-task">
+            <input
+              type="text"
+              onChange={this.changeHandler}
+              value={this.state.currentTask}
+              placeholder="add your tasks....."
+            />
+            <button className="add-task" onClick={this.addHandler}>
+              Add Task
+            </button>
+            <button className="reset-task" onClick={this.inputReset}>
+              Clear Input
+            </button>
+          </div>
+
+          <div className="tasks">
+            <div className="todo">
+              <h3>ToDo tasks</h3>
+              <ul>
+                {this.state.todoList.map((item) => (
+                  <li
+                    key={this.state.todoList.indexOf(item)}
+                    onClick={() =>
+                      this.deleteHandler(this.state.todoList.indexOf(item))
+                    }
+                  >
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="completed">
+              <h3>Completed tasks</h3>
+              <ul>
+                {this.state.completedList.map((item) => (
+                  <li key={this.state.completedList.indexOf(item)}>{item}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
